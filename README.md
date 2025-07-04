37105c76e057ca83efc05d870769e8ae229c4fab
Đinh Cao Minh Quân <minhquandinhcao@gmail.com>
2025-07-05 01:46:45 +0700

    Fix statement format and update README.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Blue Whale
 
 A Hoshino-style Discord bot
+
+## Features
+
+-   Casual chat
+-   Find information on Google to answer questions
+-   Process images/YouTube videos
+-   Add/Cancel reminders
+-   Register/Unregister for LeetCode Daily Challenge
+-   Manage LeetCode Daily Challenge threads
--- handlers/LeetcodeHandler.js
+++ handlers/LeetcodeHandler.js
@@ -1,15 +1,14 @@
-import { ChannelType, ThreadAutoArchiveDuration } from "discord.js";
+import { ChannelType, Message, ThreadAutoArchiveDuration } from "discord.js";
 import client from "../index.js";
 import { MessageUtils } from "../utils/MessageUtils.js";
 import dotenv from "dotenv";
+import TurndownService from "turndown";
 
 dotenv.config();
 const LEETCODE_CHANNEL_ID = process.env.LEETCODE_CHANNEL_ID;
 
 export class LeetcodeHandler {
     constructor(services) {
-        this.chatService = services.chatService;
-        this.historyService = services.historyService;
         this.leetcodeService = services.leetcodeService;
     }
 
@@ -35,8 +34,6 @@ export class LeetcodeHandler {
             }
         }
 
-        const prompt = `Tell the me about today's Leetcode Daily Challenge:\n\n**ID:** ${data.question.questionFrontendId}\n**Title:** ${data.question.title}\n**Difficulty:** ${data.question.difficulty}\n**Statement (in html format):** ${data.question.content}\n\nLink: https://leetcode.com${data.link}\n\nPlease format your response in Discord Markdown format, include EVERY information provided but do not provide any solution or comment to the question. Remember to add the question ID follows by a dot and a space before the title (like "1. Two Sum"). Do not forget to add the source link at the end of the question. Finally, the response should be human-readable and do not contain any html element.\n\nFollow this example carefully:\n# 1. Two Sum\n## Difficulty: Easy\n## Statement\nGiven an array of integers, return indices of the two numbers such that they add up to a specific target. You may assume that each input would have exactly one solution, and you may not use the same element twice.\n## Example\n### Example 1\n**Input:** \`nums = [2,7,11,15], target = 9\`\n**Output:** \`[0,1]\`\n**Explanation:** \`Because nums[0] + nums[1] == 9, we return [0, 1]\`.\n### Example 2\n**Input:** \`nums = [3,2,4], target = 6\`\n**Output:** \`[1,2]\`\n### Example 3\n**Input:** \`nums = [3,3], target = 6\`\n**Output:** \`[0,1]\`\n##Constraints\n- \`2 <= nums.length <= 10^4\`\n- \`-10^9 <= nums[i] <= 10^9\`\n- \`-10^9 <= target <= 10^9\`\n- Only one valid answer exists.\n## [Source](https://leetcode.com/problems/two-sum)`;
-
         const channel = await client.channels.fetch(LEETCODE_CHANNEL_ID);
         const threadName = `${data.question.questionFrontendId}. ${data.question.title}`;
         const availableTags = channel.availableTags;
@@ -61,19 +58,17 @@ export class LeetcodeHandler {
             appliedTags: [tagToId[data.question.difficulty]],
         });
 
-        this.historyService.initHistory(this.thread.id);
-        const chat = this.chatService.startChat({
-            history: this.historyService.getHistory(this.thread.id).messages,
-            tools: [{ google_search: {} }],
-        });
+        let content = this.formatContent(data.question.content);
+        content =
+            `# ${data.question.title}\n\n## Difficulty: ${data.question.difficulty}\n\n## Statement\n\n` +
+            content +
+            `\n\n## [Link to the question](https://leetcode.com${data.link})`;
 
-        const result = await chat.sendMessage(prompt);
-        const response = result.response.text();
-
-        const messages = MessageUtils.splitMessage(`${response}`);
+        const messages = MessageUtils.splitMessage(content);
         for (const message of messages) {
             await this.thread.send({
                 content: message,
+                allowedMentions: { users: [] },
             });
         }
 
@@ -132,13 +127,57 @@ export class LeetcodeHandler {
                 const user = submissionData.user;
                 const submission = submissionData.submission;
 
+                const solution =
+                    submission.statusDisplay === "Accepted"
+                        ? `[solution](https://leetcode.com${submission.url})`
+                        : "solution";
                 await this.thread.send({
-                    content: `<@${user.userId}> submitted <t:${submission.timestamp}:R> and got **[${submission.statusDisplay}](https://leetcode.com${submission.url})**.`,
+                    content: `<@${user.userId}> submitted a ${solution} <t:${submission.timestamp}:R> and got **${submission.statusDisplay}**`,
                     allowedMentions: { users: [] },
                 });
             }
 
             lastUpdate.setTime(newUpdate.getTime());
         }, 60 * 1000);
+    }
+
+    formatContent(content) {
+        let result = content
+            .replace(/\\\\u/g, "\\u")
+            .replace(/\\u[\dA-Fa-f]{4}/g, (m) =>
+                String.fromCharCode(parseInt(m.slice(2), 16))
+            )
+            .replace(/\\n/g, "\n")
+            .replace(/\\t/g, "\t")
+            .replace(/&lt;/g, "<")
+            .replace(/&gt;/g, ">")
+            .replace(/&nbsp;/g, " ")
+            .replace(/<sup>(\d+)<\/sup>/g, "^$1");
+
+        const turndownService = new TurndownService();
+        result = turndownService.turndown(result);
+
+        result = result
+            .replace(/\*\*Example (\d+):\*\*/g, "### Example $1")
+            .replace(/^(### Example 1)/m, "## Example\n\n$1")
+            .replace(/\*\*Constraints:\*\*/g, "## Constraints")
+            .replace(/\*\*Follow-up:\*\* /g, "## Follow-up\n\n")
+            .replace(
+                /^\*\*Input:\*\*\s*(.+)$/gm,
+                (_, content) =>
+                    `**Input:** \`${content.replace(/\\([\[\]])/g, "$1")}\``
+            )
+            .replace(
+                /^\*\*Output:\*\*\s*(.+)$/gm,
+                (_, content) =>
+                    `**Output:** \`${content.replace(/\\([\[\]])/g, "$1")}\``
+            )
+            .replace(
+                /^\*\*Explanation:\*\*\s*(.+)$/gm,
+                (_, content) =>
+                    `**Explanation:** ${content.replace(/\\([\[\]])/g, "$1")}`
+            );
+
+        return result;
     }
 }
